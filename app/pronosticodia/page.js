0c19caf7eeac14bfdadf6138905adf8f8bec7f2a
Cristhian Villa <crvm1993@gmail.com>
2023-07-07 13:52:59 -0500

    bug wather

--- app/pronosticodia/page.js
+++ app/pronosticodia/page.js
@@ -1,27 +1,46 @@
 import './pronosdia.css'
 
-const WeatherForecast = ({ forecast, getFormattedDate }) => (
-    <>
-      
-      {forecast.map((item) => (
-        <div key={item.dt} className='dia'>
-          <p>{getFormattedDate(item.dt_txt).substring(0, 17)}</p>
-          
-          {item.weather.map((weather) => (
-            <div key={weather.id} className='imgdias'>
-              <img
-                src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
-                alt={weather.description}
-              />
-              
-            </div>
-            
-          ))}
-          <p><span id='span1'>{item.main.temp.toFixed(0)}°C</span> <span id='span2'>11°C</span></p>
-          
-        </div>
-      ))}
-    </>
-  );
+
+  import React from 'react'
   
-  export default WeatherForecast;
+  export default function WeatherForecast({ forecast, getFormattedDate }) {
+    console.log("hola")
+    console.log(forecast)
+    return (
+      <>
+     
+    {forecast.map((item) => (
+      <div key={item.dt} className='dia'>
+        <p>{getFormattedDate(item.dt_txt).substring(0, 17)}</p>
+        
+        {/* {item.weather.map((weather) => (
+          <div key={weather.id} className='imgdias'>
+            <img
+              src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
+              alt={weather.description}
+            />
+            
+          </div>
+          
+        ))} */}
+
+          <div key={item.weather[0].id} className='imgdias'>
+            <img
+              src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+              alt={item.weather[0].description}
+            />
+            
+          </div>
+          
+      
+
+
+       {/*  hasta qui */}
+        <p><span id='span1'>{item.main.temp.toFixed(0)}°C</span> <span id='span2'>11°C</span></p>
+        
+      </div>
+    ))}
+  </>
+    )
+  }
+  
